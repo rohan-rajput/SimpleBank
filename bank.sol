2bf971110c90c7d3c6587bb2398b41e2612a58d0
Rohan Singh Rajput <43549381+rohan-rajput@users.noreply.github.com>
2021-12-25 11:24:59 +0530

    initial commit

---
+++ bank.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract bank{
+    mapping (address=>uint) private balances;
+    address owner;
+
+    event LogDepositMade(address accountAddress,uint amount);
+
+    constructor(){
+        owner  = msg.sender;
+    }
+
+    function deposit() public payable returns(uint) {
+         // Use 'require' to test user inputs, 'assert' for internal invariants
+        // Here we are making sure that there isn't an overflow issue
+        require((balances[msg.sender]+msg.value)>=balances[msg.sender]);
+        balances[msg.sender]+=msg.value;
+        emit LogDepositMade(msg.sender,msg.value);
+        return msg.value;
+    } 
+
+     function withdraw(uint withdrawAmount) public returns (uint remainingBalance) {
+         require(withdrawAmount<=balances[msg.sender]);
+         balances[msg.sender] -=withdrawAmount;
+         payable(msg.sender).transfer(withdrawAmount);
+         return balances[msg.sender];
+     }
+
+    function balance()view public returns(uint) {
+        return balances[msg.sender];
+    }
+
+}
